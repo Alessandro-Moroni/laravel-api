ba5c68dddd783cdcd354a4327bcbef8366a0921e
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-04 18:32:04 +0200

    [add]type-show.project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -20,12 +20,17 @@
 
     @endif
 
+    <p>Types:
+        <span class="badge rounded-pill text-bg-success">{{ $project->type->title }}</span>
+    </p>
+
     @if ($project->image)
         <img src="{{ asset('storage/'. $project->image) }}"  alt="{{$project->image}}" width="100" class="project-img">
 
     @else
     <img src="/img/not-found.jpg"  alt="" width="100" class="project-img">
     @endif
+
 
 
 </div>
